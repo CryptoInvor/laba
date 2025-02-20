70eef4e830f3f37530421ed23bc1a914d5bbb539
CryptoInvor <invoronkin@gmail.com>
2025-02-20 22:05:20 +0300

    Add files via upload

--- laba4/laba4.cpp
+++ laba4/laba4.cpp
@@ -6,22 +6,22 @@ using namespace std;
 bool task1(char words[], char sub[], int subword, int word) { // функция 1
 	char sub1[100]{};
 	bool flag = false;
-	int count = 0, count2 = subword, count3 = 0;
+	int count = 0, count2 = subword, count3 = 0, count4 = 0;
 	for (int i = 0; i < count2; i++) {
-		for (int w = i; w < count2-1; w++) {
-			if (sub[i] == sub[w+1]) {
+		for (int w = i+1; w < count2; w++) {
+			if (sub[i] == sub[w]) {
 				subword -= 1;
 				flag = true;
 				break;
 			}
 		}
 		if (!flag) {
-			sub1[i] = sub[i];
+			sub1[count4] = sub[i];
+			count4 += 1;
 		}
 		else {
 			flag = false;
 		}
-		cout << flag << endl;
 	}
 	for (int i = 0; i < word; i++) { // перебор строки и подстроки
 		for (int j = 0; j < subword; j++) {
@@ -48,12 +48,30 @@ bool task1(char words[], char sub[], int subword, int word) { // функция 
 
 
 bool task2(char words[], char sub[], int subword, int word) { // функция 2
-	int counter = 0;
+	char sub1[100]{};
+	bool flag = false;
+	int count = 0, count2 = subword, count3 = 0, count4 = 0, counter = 0;
+	for (int i = 0; i < count2; i++) {
+		for (int w = i + 1; w < count2; w++) {
+			if (sub[i] == sub[w]) {
+				subword -= 1;
+				flag = true;
+				break;
+			}
+		}
+		if (!flag) {
+			sub1[count4] = sub[i];
+			count4 += 1;
+		}
+		else {
+			flag = false;
+		}
+	}
 	
 	if (word >= subword) {
 		for (int i = 0; i <= (word - subword); i++) {
 			for (int j = 0; j < subword; j++) {
-				if (words[i + j] == sub[j]) {
+				if (words[i + j] == sub1[j]) {
 					counter += 1;
 				}
 			}
@@ -73,16 +91,16 @@ bool task2(char words[], char sub[], int subword, int word) { // функция 
 }
 
 
-vector<string> split(string& s, string& delimiter) {
+vector<string> split(const string s, const string delimiter) {
 	vector<string> tokens;
 	size_t pos = 0;
 	string token;
-	while ((pos = s.find(delimiter)) != string::npos) {
-		token = s.substr(0, pos);
-		tokens.push_back(token);
-		s.erase(0, pos + delimiter.length());
-	}
-	tokens.push_back(s);
+	size_t last = 0; 
+	size_t next = 0; 
+	while ((next = s.find(delimiter, last)) != string::npos) { 
+		tokens.push_back(s.substr(last, next - last));   
+		last = next + 1; 
+	} tokens.push_back(s.substr(last));
 
 	return tokens;
 }
@@ -105,7 +123,7 @@ switch (stoi(argv[1])) {
 		cin.getline(words, charlength);
 		cout << "Введите искомую подстроку:" << endl;
 		cin >> subwords;
-		for (int i = 0; subwords[i] != taskwords[i]; i++) { // подсчет длинны подстроки
+		for (int i = 0; subwords[i] != (char)0; i++) { // подсчет длинны подстроки
 			subword += 1;
 		}
 		for (int i = 0; i < charlength; i++) { // подсчет длинны строки
@@ -127,13 +145,13 @@ switch (stoi(argv[1])) {
 					cout << "В слове " << taskwords << " нет всех символов слова " << subwords << endl; // вывод если false
 				}
 				for (int k = 0; k < counter; k++) {
-					taskwords[k] = ' ';
+					taskwords[k] = (char)0;
 				}
 				counter = 0;
 			}
 		}
 		for (int k = 0; k < counter; k++) { // обнуление переменных
-			taskwords[k] = ' ';
+			taskwords[k] = (char)0;
 		}
 		counter = 0;
 		space = 0;
@@ -143,6 +161,9 @@ switch (stoi(argv[1])) {
 			if ((words[p] != ' ') && (words[p] != '\0')) {
 				counter += 1;
 				taskwords[p - siz] = words[p];
+				if (words[p + 1] == (char)0) {
+					words[p + 1] = (char)32;
+				}
 			}
 			else {
 				space += 1;
@@ -152,11 +173,11 @@ switch (stoi(argv[1])) {
 					for (int m = 0; space < length; m++) {
 						if (m < counter + 1) {
 							finale[m] = words[m + (siz - counter - 1)];
-							if (words[m + (siz - counter - 1)] == ' ') space += 1;
+							if (words[m + (siz - counter - 1)] == (char)32) space += 1;
 						}
 						else if (((m - counter) < (siz - counter - 1))) {
 							finale[m] = words[m - counter - 1];
-							if (words[m - counter - 1] == ' ') space += 1;
+							if (words[m - counter - 1] == (char)32) space += 1;
 						}
 						else {
 							finale[m] = words[m];
@@ -167,10 +188,10 @@ switch (stoi(argv[1])) {
 					return 0;// вывод если true
 				}
 				else {
-					// вывод если false
+					//cout << "подстроки " << subwords << " не обнаружено в данной строке" << endl;
 				}
 				for (int k = 0; k < counter; k++) {
-					taskwords[k] = ' ';
+					taskwords[k] = (char)0;
 				}
 				counter = 0;
 			}
@@ -182,17 +203,13 @@ switch (stoi(argv[1])) {
 		cout << "Введите слова через пробел:" << endl; // ввод строки и подстроки
 		cin.getline(words, charlength);
 		cout << "Введите разделяющую подстроку:" << endl;
-		cin >> subwords;
-		for (int i = 0; subwords[i] != taskwords[i]; i++) { // подсчет длинны подстроки
-			subword += 1;
-		}
+		cin >> subwords1;
 		for (int i = 0; i < charlength; i++) { // подсчет длинны строки
 			if (words[i] != (char)0) { 
 				length += 1;
 				words1 += words[i];
 			}
 		}
-		cout << words << endl;
 		vector<string> result = split(words1, subwords1);
 		cout << "разделенный массив: ";
 		for(int q = 0; q < result.size(); q++){
@@ -200,7 +217,7 @@ switch (stoi(argv[1])) {
 				cout << result[q] << ", ";
 			}
 			else {
-				cout << result[q] << ", ";
+				cout << result[q];
 			}
 		}
 	}break;
---
+++ laba6/laba6.cpp
@@ -0,0 +1,116 @@
+﻿
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include<cstdlib>
+using namespace std;
+
+vector<int> fill(){
+    srand(time(NULL));
+    vector<int> s;
+    cout << "Вектор состоит из следующих элементов: ";
+    for (int i = 0; i < 50; i++) {
+        s.push_back(rand() % 100 + 1);
+        
+        if (i != 49) {
+            cout << s[i] << ", ";
+        }
+        else {
+            cout << s[i] << endl;
+        }
+    }
+    return s;
+}
+
+
+vector<int> middle(vector<int> s, int k) {
+    int sum = 0;
+    for (int i = 0; i < s.size(); i++) {
+        sum += s[i];
+    }
+    sum = sum / s.size();
+    cout << "среднее значение: " << sum << endl;
+    s.insert(s.begin() + k, sum);
+    return s;
+}
+
+
+vector<int> del(vector<int> s, int k) {
+    int small = 9999999, big = 0, place = 0;
+    if (k == 0) {
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] < small) {
+                small = s[i];
+            }
+        }
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == small) {
+                place = i;
+            }
+        }
+        cout << "Наименьшее значение: " << small << endl;
+        s.erase(s.begin() + place);
+    }
+    else {
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] > big) {
+                big = s[i];
+            }
+        }
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == big) {
+                place = i;
+            }
+        }
+        cout << "Наибольшее значение: " << big << endl;
+        s.erase(s.begin() + place);
+        
+    }
+
+
+    return s;
+}
+
+
+
+
+
+
+int main()
+{
+    setlocale(LC_CTYPE,"");
+    int k, type;
+    cout << "Введите положение элемента среднего значения: ";
+    cin >> k;
+    if (k < 0){
+        cout << "Пожалуйста, введите положотиельное значение";
+        return 0;
+    }
+    cout << "Введите 0 для удаления минимального элемента, 1 для удаления максимального элеменита: ";
+    cin >> type;
+    if ((type != 0) && (type != 1)) {
+        cout << "Пожалуйста, введите 0 или 1";
+        return 0;
+    }
+    cout << "---------------------------" << endl;
+    vector<int> element;
+    element = fill();
+    cout << "---------------------------" << endl;
+    element = middle(element, k);
+    cout << "---------------------------" << endl;
+    element = del(element, type);
+    cout << "---------------------------" << endl;
+    cout << "Финальный вид вектора: ";
+    for (int i = 0; i < 50; i++) {
+        if (i != 49) {
+            cout << element[i] << ", ";
+        }
+        else {
+            cout << element[i] << endl;
+        }
+    }
+    return 0;
+}
+
+
