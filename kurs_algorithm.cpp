ffb07cfa145c1fe55a6efe42ca88cb0bcd1abcf3
CryptoInvor <invoronkin@gmail.com>
2025-04-10 23:39:26 +0300

    Add files via upload

--- kurs_algorithm.cpp
+++ kurs_algorithm.cpp
@@ -16,59 +16,9 @@ struct rect {
 };
 
 
-static vector<rect> del_rep(vector<rect> rects){
-    rect cur;
-    vector<rect> temp_rects = rects;
-    for (int i = 0; i < rects.size(); i++) {
-        int match = 0;
-        
-        for (int j = 0; j < temp_rects.size(); j++) {
-            dot dot1 = rects[i].first;
-            dot dot2 = rects[i].second;
-            dot dot3 = rects[i].third;
-            dot dot4 = rects[i].fourth;
-            int count = 1;
-            for (int p = 0; p < 4; p++) {
-                switch (count) {
-                case 1:
-                    if (dot1.x == temp_rects[j].first.x && dot1.y == temp_rects[j].first.y) { match++; }
-                    if (dot1.x == temp_rects[j].second.x && dot1.y == temp_rects[j].second.y) { match++; }
-                    if (dot1.x == temp_rects[j].third.x && dot1.y == temp_rects[j].third.y) { match++; }
-                    if (dot1.x == temp_rects[j].fourth.x && dot1.y == temp_rects[j].fourth.y) { match++; }
-                    count++;
-                    break;
-                case 2:
-                    if (dot2.x == temp_rects[j].first.x && dot2.y == temp_rects[j].first.y) { match++; }
-                    if (dot2.x == temp_rects[j].second.x && dot2.y == temp_rects[j].second.y) { match++; }
-                    if (dot2.x == temp_rects[j].third.x && dot2.y == temp_rects[j].third.y) { match++; }
-                    if (dot2.x == temp_rects[j].fourth.x && dot2.y == temp_rects[j].fourth.y) { match++; }
-                    count++;
-                    break;
-                case 3:
-                    if (dot3.x == temp_rects[j].first.x && dot3.y == temp_rects[j].first.y) { match++; }
-                    if (dot3.x == temp_rects[j].second.x && dot3.y == temp_rects[j].second.y) { match++; }
-                    if (dot3.x == temp_rects[j].third.x && dot3.y == temp_rects[j].third.y) { match++; }
-                    if (dot3.x == temp_rects[j].fourth.x && dot3.y == temp_rects[j].fourth.y) { match++; }
-                    count++;
-                    break;
-                case 4:
-                    if (dot4.x == temp_rects[j].first.x && dot4.y == temp_rects[j].first.y) { match++; }
-                    if (dot4.x == temp_rects[j].second.x && dot4.y == temp_rects[j].second.y) { match++; }
-                    if (dot4.x == temp_rects[j].third.x && dot4.y == temp_rects[j].third.y) { match++; }
-                    if (dot4.x == temp_rects[j].fourth.x && dot4.y == temp_rects[j].fourth.y) { match++; }
-                    count++;
-                    break;
-                }
-            }
-            if (match == 4) {
-
-            }
-        }
-    }
-}
-
-
 static void print_rect(vector<rect> rects) {
+    cout << "-----------------------------------" << endl;
+    cout << "Прямоугольник номер 1" << endl;
     for (int i = 0; i < rects.size(); i++) {
         cout << "(" << rects[i].third.x << "," << rects[i].third.y << ")" << "          " << "(" << rects[i].fourth.x << "," << rects[i].fourth.y << ")" << endl;
         cout << endl;
@@ -76,6 +26,7 @@ static void print_rect(vector<rect> rects) {
         cout << endl;
         cout << "(" << rects[i].first.x << "," << rects[i].first.y << ")" << "          " << "(" << rects[i].second.x << "," << rects[i].second.y << ")" << endl;
         cout << "-----------------------------------" << endl;
+        if (i != rects.size()-1) { cout << "Прямоугольник номер " << i + 2 << endl; }
     }
 }
 
@@ -111,8 +62,9 @@ static vector<dot> init() {
 
 static vector<dot> sort_x(vector<dot> sorted) {
     vector<dot> result;
+    int smallest = sorted[0].x;
     for (int j = 0; j < sorted.size();) {
-        int smallest = sorted[0].x;
+        
         for (int i = 0; i < sorted.size(); i++) {
             if (sorted[i].x < smallest) { smallest = sorted[i].x; } 
         }
@@ -150,7 +102,7 @@ static vector<rect> search(vector<dot> bsearch) {
     vector<dot> possible;
     vector<rect> result;
     rect temp_r;
-    for (int i = 0; i < bsearch.size(); i++) {
+    for (int i = 0; i < bsearch.size()-1; i++) {
         width.clear();
         possible.clear();
         temp_d = bsearch[i];
@@ -168,10 +120,10 @@ static vector<rect> search(vector<dot> bsearch) {
                     possible.push_back(poss_d);
                 }
             }
-
-            for (int j = 0; j < possible.size(); j++) {
-                for (int o = 0; o < temp_search.size(); o++) {
-                    if ((possible[j].x == temp_search[o].x) && (possible[j].y == temp_search[o].y)) {
+        }
+        for (int j = 0; j < possible.size(); j++) {
+            for (int o = 0; o < temp_search.size(); o++) {
+                if ((possible[j].x == temp_search[o].x) && (possible[j].y == temp_search[o].y)) {
                         final_rect.first = temp_d;
                         final_rect.second.y = temp_d.y;
                         final_rect.second.x = possible[j].x;
@@ -179,10 +131,10 @@ static vector<rect> search(vector<dot> bsearch) {
                         final_rect.third.y = possible[j].y;
                         final_rect.fourth = possible[j];
                         result.push_back(final_rect);
-                    }
                 }
             }
         }
+        
     }
     return result;
 }
